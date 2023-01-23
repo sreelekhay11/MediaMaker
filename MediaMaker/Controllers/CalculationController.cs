0af7ea5132f44ae1d23b861282baf0a94f502c3d
sreelekhay11 <sreelekhareddy11@gmail.com>
2023-01-23 16:58:41 +0000

    Add project files.

---
+++ MediaMaker/Controllers/CalculationController.cs
@@ -0,0 +1,52 @@
+﻿using MediaMaker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace MediaMaker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculationController : ControllerBase
+    {
+        [HttpPost("add")]
+        public IActionResult Add(Calculation calculation)
+        {
+            calculation.Result = calculation.Number1 + calculation.Number2;
+            var response = JsonSerializer.Serialize(calculation.Result);
+            return Ok(response);
+        }
+
+        [HttpPost("subtract")]
+        public IActionResult Sub(Calculation calculation)
+        {
+            calculation.Result = calculation.Number1 - calculation.Number2;
+            var response = JsonSerializer.Serialize(calculation.Result);
+            return Ok(response);
+        }
+
+        [HttpPost("multipy")]
+        public IActionResult Mul(Calculation calculation)
+        {
+            calculation.Result = calculation.Number1 * calculation.Number2;
+            var response = JsonSerializer.Serialize(calculation.Result);
+            return Ok(response);
+        }
+
+        [HttpPost("division")]
+        public IActionResult Div(Calculation calculation)
+        {
+            try
+            {
+                calculation.Result = calculation.Number1 / calculation.Number2;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            var response = JsonSerializer.Serialize(calculation.Result);
+            return Ok(response);
+        }
+
+    }
+}
---
+++ MediaMaker/Models/Calculation.cs
@@ -0,0 +1,9 @@
+﻿namespace MediaMaker.Models
+{
+    public class Calculation
+    {
+        public double Number1 { get; set; }    
+        public double Number2 { get; set; }    
+        public double Result { get; set; }
+    }
+}
